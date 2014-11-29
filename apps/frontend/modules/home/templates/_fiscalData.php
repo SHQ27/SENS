69d04fa49ae7ef447a4f8c243e3001fc1b21d5c0
Emanuel Mahler <EmanuelMahler@Emanuels-MacBook-Pro.local>
2014-11-29 15:34:26 -0300

    agregué el modal de la foto de Data Fiscal

---
+++ apps/frontend/modules/home/templates/_fiscalData.php
@@ -0,0 +1,11 @@
+<div id="dataFiscal" class="modal fade" aria-labelledby="dataFiscalModal" role="dialog" aria-hidden="true" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class='form-top__modal datafiscal-modal'>
+                    <img class="datafiscal-modal-img" src="img/dataFiscalBig.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>      
--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -69,15 +69,15 @@
         };
         </script>
         <script type="text/javascript">
-var $_Tawk_API={},$_Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/53848db9e33ac3a8210000ad/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+            var $_Tawk_API={},$_Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/53848db9e33ac3a8210000ad/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
 </script>
     </head>
     <body onload='initializeDerqui(-34.4617896, -58.8594284); initializeViso( -34.4439678,-58.7864617)'>
@@ -344,7 +344,7 @@ s0.parentNode.insertBefore(s1,s0);
             <a href="http://www.ssn.gov.ar/" target="_blank"><img class="pie-ssn" src="img/ssn.png"></a>
         </div>
         <div class="pie-data-fiscal col-xs-6 col-sm-6 col-md-6">
-            <a href="#!"><img class="data-fiscal-img" src="img/dataFiscalBig.jpg"></a>
+            <a href="#!" data-toggle='modal' data-target='#dataFiscal'><img class="data-fiscal-img" src="img/dataFiscalBig.jpg"></a>
         </div>
     </div>
 
@@ -364,6 +364,7 @@ s0.parentNode.insertBefore(s1,s0);
         </div>
     </footer>
     <?php include_partial('home/modal') ?>
+    <?php include_partial('home/fiscalData') ?>
     <script>seguros();</script>
     <script>contacto();</script>
     <script>siniestro();</script>
